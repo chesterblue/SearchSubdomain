36ad58707e02ca779e47c3bac97a6188daf1d0ef
chesterblue <1191975374@qq.com>
2020-08-01 00:58:07 +0800

    Fix bugs and add the function of saving results

--- sdomlookup.py
+++ sdomlookup.py
@@ -1,5 +1,8 @@
+import os
 from requests import head
 from requests import session
+'''global variate'''
+known_subdomain=[]
 
 '''
   读取字典文件中的内容
@@ -12,17 +15,66 @@ def get_dict_contents(filename):
       subdomain.append(line)
   return subdomain
 
-def request_head(url,s):
-  code=head(url).status_code
-  if code==200:
-    print(url+" [*]")
+'''
+  https协议
+'''
+def request_head_s(url):
+  code=0
+  try:
+    code=head(url,timeout=3).status_code
+  except KeyboardInterrupt:
+    os._exit(0)
+  except:
+    pass
+  else:
+    return code
 
-def main(url,filename):
-  subdomain=get_dict_contents(filename)
-  s=session()
+'''
+  http协议
+'''
+def request_head(url):
+  code=0
+  try:
+    code=head(url,timeout=3).status_code
+  except KeyboardInterrupt:
+    os._exit(0)
+  except:
+    pass
+  finally:
+    return code
+
+'''
+  结果保存到文件
+'''
+def write_into_file(filename):
+  with open(filename,"a+") as fp:
+    for subdomain in known_subdomain:
+      fp.write(subdomain+"\n")
+
+'''
+  循环测试URL
+'''
+def loop_connect_site(domain,subdomain):
+  global known_subdomain
   for sdom in subdomain:
-    url="https://"+sdom+"."+url+"/"
-    request_head(url,s)
+    url_s="https://%s.%s/"%(sdom,domain)
+    url="http://%s.%s/"%(sdom,domain)
+    print(url_s+"......")
+    code=request_head_s(url_s)
+    if code==200:
+      print(url_s+"[*]")
+      known_subdomain.append(url_s)
+    else:
+      print(url+"......")
+      code=request_head(url)
+      if code==200:
+        print(url+"[*]")
+        known_subdomain.append(url)
+
+def main(domain,filename):
+  subdomain=get_dict_contents(filename)
+  loop_connect_site(domain,subdomain)
+  write_into_file(domain+".txt")
 
 if __name__=="__main__":
-  main("baidu.com","./dict/common.txt")
+  main("douyu.com","./dict/common.txt")
